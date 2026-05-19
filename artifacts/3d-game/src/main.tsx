9be59c5eed3c98647e873d94a6b9879685fe0b73
Kacper Bernecki <ft4k696bk6@privaterelay.appleid.com>
2026-05-19 19:43:58 +0200

    Remove React Speed Insights integration

--- artifacts/3d-game/src/main.tsx
+++ artifacts/3d-game/src/main.tsx
@@ -1,11 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <SpeedInsights />
-  </>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
